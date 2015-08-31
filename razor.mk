a1e59a0cec31b2bce38e2d978b18c9ca5303ab07
vibhu0009 <cvibhu0009@gmail.com>
2015-08-31 20:27:39 +0200

    Bringup RAZOR for tomato

--- razor.mk
+++ razor.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Teah-Razor stuff.
+$(call inherit-product, vendor/razor/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME := razor_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
